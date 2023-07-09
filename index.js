cbdf8da125a2ebc7270def6839e6a497a56bb72f
muganga-charles <mugangacharls5@gmail.com>
2023-07-09 18:14:32 +0300

    initial

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
+const express = require('express');
+const cors = require("cors");
+const mysql = require("mysql");
 const app = express()
+app.use(cors())
+app.use(express.json())
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3900)
