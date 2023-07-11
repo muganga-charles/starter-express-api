e8cd71bceab4d73ff7989170c7c502472201fac2
muganga-charles <mugangacharls5@gmail.com>
2023-07-11 19:16:59 +0300

    init

--- index.js
+++ index.js
@@ -11,9 +11,21 @@ const database = {
     password: "gtnLbfgucC",
     database: "sql8632033",
 };
+const pool = mysql.createPool(database);
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3900)
+
+app.get("/api/data", (req, res) => {
+    const query = "SELECT * FROM signup;"; 
+
+    pool.query(query, (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: "Error retrieving data from database." });
+        }
+        return res.json(results);
+    });
+});
